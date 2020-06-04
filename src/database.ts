360b6fad6873d178d8dd1da93ed88bc714e3111c
HamishBrindle <hamishbrindle@gmail.com>
2020-06-03 17:58:59 -0700

    Add ability to pass plugins to VuexORM

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,16 @@
 import VuexORM, { Model, Database } from '@vuex-orm/core';
 import { Plugin } from 'vuex';
 
-
 export class ORMDatabase {
-
     private static _ormDatabase = new VuexORM.Database();
+    
     private static _installed = <typeof Model[]>[];
 
-    public static install(): Plugin<any> {
+    public static install(options: any): Plugin<any> {
+        const plugins = options.plugins;
+        if (plugins && plugins.length) {
+            plugins.forEach((plugin: any) => VuexORM.use(plugin))
+        }
         return VuexORM.install(ORMDatabase._ormDatabase);
     }
 
